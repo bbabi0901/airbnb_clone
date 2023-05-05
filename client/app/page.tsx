7cff986dda8e0cbad3e947d4d7f90859122ba39b
bbabi0901 <69451349+bbabi0901@users.noreply.github.com>
2023-05-05 16:10:16 +0900

    Fix: ADD api for favorites

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -25,13 +25,13 @@ export default async function Home() {
         <div
           className="
             pt-24
-            grid
-            grid-cols-1
-            sm:grid-col-2
-            md:grid-col-3
-            lg:grid-col-4
-            xl:grid-col-5
-            2xl:grid-col-6
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
             gap-8
           "
         >
