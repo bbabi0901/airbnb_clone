d0fcb009ae7cfa846ccc58c2060543419267764e
bbabi0901 <69451349+bbabi0901@users.noreply.github.com>
2023-05-05 16:30:59 +0900

    Fix: ADD hook useFavorite

---
+++ client/app/hooks/useFavorite.ts
@@ -0,0 +1,62 @@
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useCallback, useMemo } from "react";
+import { toast } from "react-hot-toast";
+
+import { SafeUser } from "../types";
+
+import useLoginModal from "./useLoginModal";
+import LoginModal from "../components/modals/LoginModal";
+
+interface IUseFavorite {
+  listingId: string;
+  currentUser?: SafeUser | null;
+}
+
+const useFavorite = ({ listingId, currentUser }: IUseFavorite) => {
+  const router = useRouter();
+  const loginModal = useLoginModal();
+
+  const hasFavorited = useMemo(() => {
+    const list = currentUser?.favoriteIds || [];
+
+    return list.includes(listingId);
+  }, [currentUser, listingId]);
+
+  const toggleFavorite = useCallback(
+    async (e: React.MouseEvent<HTMLDivElement>) => {
+      e.stopPropagation();
+
+      if (!currentUser) {
+        return loginModal.onOpen();
+      }
+
+      try {
+        let request;
+
+        // user data의 favorite에 관한 리스트에 listingId가 있으면 = hasFavorited가 true면
+        // send request of "delete"
+        // 아니면 send request of "post"
+        if (hasFavorited) {
+          request = () => axios.delete(`/api/favorites/${listingId}`);
+        } else {
+          request = () => axios.post(`/api/favorites/${listingId}`);
+        }
+
+        await request();
+        router.refresh();
+        toast.success("Success");
+      } catch (error) {
+        toast.error("Something went wrong.");
+      }
+    },
+    [currentUser, hasFavorited, router, loginModal, listingId]
+  );
+
+  return {
+    hasFavorited,
+    toggleFavorite,
+  };
+};
+
+export default useFavorite;
