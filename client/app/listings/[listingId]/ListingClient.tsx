db26b848f96f653de2fa8634d60540f50496fd3b
bbabi0901 <bbabi0901@gmail.com>
2023-05-13 13:52:53 +0900

    Feat: apply type 'SafeReservation'

--- client/app/listings/[listingId]/ListingClient.tsx
+++ client/app/listings/[listingId]/ListingClient.tsx
@@ -10,7 +10,7 @@ import { Range } from "react-date-range";
 
 import useLoginModal from "@/app/hooks/useLoginModal";
 
-import { SafeUser, SafeListing } from "@/app/types";
+import { SafeUser, SafeListing, SafeReservation } from "@/app/types";
 import { categories } from "@/app/components/navbar/Categories";
 import Container from "@/app/components/Container";
 import ListingHead from "@/app/components/listings/ListingHead";
@@ -24,7 +24,7 @@ const initialDateRange = {
 };
 
 interface ListingClientProps {
-  reservations?: Reservation[];
+  reservations?: SafeReservation[];
   listing: SafeListing & {
     user: SafeUser;
   };
--- client/app/types/index.ts
+++ client/app/types/index.ts
@@ -1,7 +1,7 @@
 // 서버 컴포넌트에서 클라이언트 컴포넌트로 데이터 이동시 데이트 타입 등에 의해 에러가 발생한다.
 // 따라서 타입을 바꿔줄 필요가 있음. (이거는 actions 등에서 실행됨.)
 // 여기서는 props에 타입 지정할때 쓸 타입을 정의해줘야함.
-import { User, Listing } from "@prisma/client";
+import { User, Listing, Reservation } from "@prisma/client";
 
 export type SafeUser = Omit<
   User,
@@ -14,4 +14,14 @@ export type SafeUser = Omit<
 
 export type SafeListing = Omit<Listing, "createdAt"> & {
   createdAt: string;
+};
+
+export type SafeReservation = Omit<
+  Reservation,
+  "createdAt" | "startDate" | "endDate" | "listing"
+> & {
+  createdAt: string;
+  startDate: string;
+  endDate: string;
+  listing: SafeListing;
 };
